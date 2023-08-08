c67b83317484726e0857b8819d86c2fa56a3abb1
sourav sambit behera <souravsambit12@gmail.com>
2023-08-08 19:52:26 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl text-lime-400'>Welcome ...</div>
+    </>
+  )
+}
+
+export default App
